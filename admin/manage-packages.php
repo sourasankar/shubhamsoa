b1f897559a67948c52a53924c2ce8c7f2498bbe7
sourasankar <64581748+sourasankar@users.noreply.github.com>
2022-04-22 10:34:05 +0530

    Update manage-packages.php

--- admin/manage-packages.php
+++ admin/manage-packages.php
@@ -95,7 +95,7 @@ else{
 						  </tr>
 						</thead>
 						<tbody>
-<?php $sql = "SELECT * from TblTourPackages";
+<?php $sql = "SELECT * from tbltourpackages";
 $query = $dbh -> prepare($sql);
 //$query -> bindParam(':city', $city, PDO::PARAM_STR);
 $query->execute();
